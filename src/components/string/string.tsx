86e8c2998892d97493662a26a87ddd557bb3277b
Daniil Mitrenin <d.mitt@yandex.ru>
2024-02-26 20:58:15 +0700

    line reversal

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from "./string.module.css";
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
+  let [string, setString] = useState("");
+
+  const handleSetString = (event: ChangeEvent<HTMLInputElement>) => {
+    setString(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const length = string.length;
+  };
+
   return (
     <SolutionLayout title="Строка">
-     
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input
+          extraClass={styles.string}
+          isLimitText={true}
+          maxLength={11}
+          value={string}
+          onChange={handleSetString}
+        ></Input>
+        <Button
+          type="submit"
+          extraClass={styles.button}
+          text={"Развернуть"}
+        ></Button>
+      </form>
+      <Circle index={1}></Circle>
     </SolutionLayout>
   );
 };
