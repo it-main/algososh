265d7277a08a04ae00560bbc07da445c6f25cae6
Daniil <d.mitt@yandex.ru>
2024-03-26 17:05:42 +0700

    add button component tests

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -9,18 +9,22 @@ describe("Тестирование компонента Button", () => {
     const { asFragment } = render(<Button text="Развернуть" />);
     expect(asFragment()).toMatchSnapshot();
   });
+
   it("Кнопка без текста рендерится корректно", () => {
     const { asFragment } = render(<Button />);
     expect(asFragment()).toMatchSnapshot();
   });
+
   it("Заблокированная кнопка рендерится корректно", () => {
     const { asFragment } = render(<Button text="Развернуть" disabled />);
     expect(asFragment()).toMatchSnapshot();
   });
+
   it("Кнопка с индикацией загрузки рендерится корректно", () => {
     const { asFragment } = render(<Button text="Развернуть" isLoader />);
     expect(asFragment()).toMatchSnapshot();
   });
+
   it("Колбек вызывается корректно", () => {
     render(<Button text="Развернуть" onClick={onClick} />);
     userEvent.click(screen.getByRole("button"));
