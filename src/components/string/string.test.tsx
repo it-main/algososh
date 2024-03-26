1a931f254d5b8b3dc36d55674676027af1acff84
Daniil <d.mitt@yandex.ru>
2024-03-26 18:21:38 +0700

    add sorting string tests

---
+++ src/components/string/string.test.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { StringComponent } from "./string";
+
+describe("Тестирование алгоритма разворота строки", () => {
+  it("should ", () => {
+    render(<StringComponent />);
+  });
+});
