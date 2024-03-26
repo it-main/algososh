fda021ea77f3ca64614c7ef2756c0280360317af
Daniil <d.mitt@yandex.ru>
2024-03-26 15:28:05 +0700

    add button component tests

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -1,15 +1,29 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Button } from "./button";
-// import renderer from "react-test-renderer";
+import userEvent from "@testing-library/user-event";
+
+const onClick = jest.fn();
 
 describe("Тестирование компонента Button", () => {
-  it("Компонент с надписью рендерится корректно", () => {
+  it("Кнопка с текстом рендерится корректно", () => {
     const { asFragment } = render(<Button text="Развернуть" />);
     expect(asFragment()).toMatchSnapshot();
-
-    // const tree = renderer
-    //   .create(<Link page="http://www.facebook.com">Facebook</Link>)
-    //   .toJSON();
-    // expect(tree).toMatchSnapshot();
+  });
+  it("Кнопка без текста рендерится корректно", () => {
+    const { asFragment } = render(<Button />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("Заблокированная кнопка рендерится корректно", () => {
+    const { asFragment } = render(<Button text="Развернуть" disabled />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("Кнопка с индикацией загрузки рендерится корректно", () => {
+    const { asFragment } = render(<Button text="Развернуть" isLoader />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  it("Колбек вызывается корректно", () => {
+    render(<Button text="Развернуть" onClick={onClick} />);
+    userEvent.click(screen.getByRole("button"));
+    expect(onClick).toBeCalled();
   });
 });
